189ab045fce8cc30bf3e9c16665769082772772e
Bastian Selman <bastian.selman@gmail.com>
2018-10-15 14:25:44 -0400

    Update python code to comply with PEP8 guidelines

--- log-analysis.py
+++ log-analysis.py
@@ -1,33 +1,41 @@
+#!/usr/bin/env python3
+
 import psycopg2
 import sys
 
+
+def printQueryResult(titleToPrint, sql, dbConn):
+    print(titleToPrint)
+    print("")
+    q = "COPY (%s) TO STDOUT WITH CSV HEADER DELIMITER '\t'" % sql
+    with dbConn.cursor() as curs:
+        curs.copy_expert(q, sys.stdout)
+    print("\n")
+
+
 # Connect to local db
-conn = psycopg2.connect("dbname=news user=vagrant")
+try:
+    conn = psycopg2.connect("dbname=news user=vagrant")
+except psycopg2.Error as e:
+    print("An error occurred while connecting to the database")
+    print("Error Number: %s" % e.pgerror)
+    print("Error Details: %s" % e.diag.message_detail)
+    sys.exit(1)
 
-#Print articles hit summarization
-print("Top Articles:\n")
-with conn.cursor() as curs:
-	q1_sql = "SELECT title, hit_count FROM article_hits"
-	q1 = "COPY (%s) TO STDOUT WITH CSV HEADER DELIMITER '\t'"%(q1_sql)
-	curs.copy_expert(q1, sys.stdout)
-print("\n")
+# Print articles hit summarization
+q1_sql = "SELECT title, hit_count FROM article_hits"
+printQueryResult("Top Articles:", q1_sql, conn)
 
-#Print author hit summarization
-print("Top Authors:\n")
-with conn.cursor() as curs:
-	q2_sql = "SELECT author_name, hit_count FROM author_hits"
-	q2 = "COPY (%s) TO STDOUT WITH CSV HEADER DELIMITER '\t'"%(q2_sql)
-	curs.copy_expert(q2, sys.stdout)
-print("\n")
+# Print author hit summarization
+q2_sql = "SELECT author_name, hit_count FROM author_hits"
+printQueryResult("Top Authors:", q2_sql, conn)
 
-#Print the days where the error rate was at or above 1%
-print("Days With Error Rate >= 1%\n")
-with conn.cursor() as curs:
-	q3_sql = "SELECT log_date, hit_fail_rate FROM daily_hit_rate_summary WHERE hit_fail_rate >= 1.0"
-	q3 = "COPY (%s) TO STDOUT WITH CSV HEADER DELIMITER '\t'"%(q3_sql)
-	curs.copy_expert(q3, sys.stdout)
-print("\n")
+# Print the days where the error rate was at or above 1%
+q3_sql = (
+        "SELECT log_date, hit_fail_rate "
+        "FROM daily_hit_rate_summary "
+        "WHERE hit_fail_rate >= 1.0:")
+printQueryResult("Days With Error Rate >= 1%", q3_sql, conn)
 
-	
-#Close the db
+# Close the db
 conn.close()
